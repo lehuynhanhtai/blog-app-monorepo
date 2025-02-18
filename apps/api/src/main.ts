a1ed78c0bb6104b05969dcb077d13816b1ad1791
lehuynhanhtai <lehuynhanhtai@gmail.com>
2025-02-18 14:55:39 +0700

    fix: fix api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  const port = process.env.PORT ?? 8000;
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
+  await app.listen(port);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  Logger.log(`🚀 Server is running: http://localhost:${port}`);
 }
 bootstrap();
