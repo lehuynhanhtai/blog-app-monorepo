29a81b685a50c88b43017ce387ace430bf577150
lehuynhanhtai <lehuynhanhtai@gmail.com>
2025-02-18 09:40:36 +0700

    init project

---
+++ apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async registerUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.registerUser(createUserDto);
+  }
+}
